4c5dac761074356c96b38305d59a29c4a08facf1
Sam Stites <stites.sr@gmail.com>
2014-04-03 22:16:58 -0700

    compressed and slightly optimized

--- untitled/src/projectEuler/_001/_001.java
+++ untitled/src/projectEuler/_001/_001.java
@@ -1,5 +1,4 @@
 package projectEuler._001;
-import java.util.ArrayList;
 
 /**
  * Created with IntelliJ IDEA.
@@ -20,7 +19,8 @@ import java.util.ArrayList;
 public class _001 {
     public static void main (String[] args){
         int sum=0;
-        for (int n=0;n<1000;n+=1) if (n%3==0 || n%5==0) sum+=n;
+        for (int n=0;n<1000;n+=3) sum+=n;
+        for (int n=0;n<1000;n+=5) if (n%3!=0) sum+=n;
         System.out.println(sum);
     }
 }
