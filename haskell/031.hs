9e8c9cb5c2a3ab0da6d6254e270075ed63921f3d
stites <sam@stites.io>
2014-12-29 02:13:04 -0500

    start - coinsums

---
+++ haskell/031.hs
@@ -0,0 +1,15 @@
+opts = [1, 2, 5, 10, 20, 50, 100, 200]
+len  = length opts
+sets = () -- where we store final results
+checkSets = -- simple check to see if val is in sets
+sumSet = -- alias to summing the set
+coinSums current count =
+  -- if sum current == 200 then
+    -- if checkSets current
+    -- then count
+    -- else
+      -- add to set
+      -- count + 1
+  -- else if sum current > 200
+    -- then count
+    -- else add an opt
