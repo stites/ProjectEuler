bc898c74944db245a616dfaf64fd8c3263795e29
stites <sam@stites.io>
2015-01-15 20:32:00 -0800

    add - prime function

--- haskell/Divisors.hs
+++ haskell/Divisors.hs
@@ -1,3 +1,12 @@
 module Divisors where
+import System.Environment (getArgs)
+import Math.NumberTheory.Primes
 divisors n = [ x | x<-[1..n `div` 2],  n `mod` x == 0]
 d = sum . divisors
+
+main = do
+  args <- getArgs
+  let lim = case args of
+        (a:_) -> read a
+        _     -> 1000000
+    print . sum $ takeWhile (<= lim) primes
