9f150203fd221cd8f80bdf5cb4bd49301342bc30
stites <sam@stites.io>
2015-01-17 23:09:54 -0800

    brute - not going to work

--- haskell/027.hs
+++ haskell/027.hs
@@ -24,3 +24,5 @@
 -- Find the product of the coefficients, a and b, for the quadratic expression
 -- that produces the maximum number of primes for consecutive values of n,
 -- starting with n = 0.
+
+ns n = [ n^n + a*n + b | a <- [-999..999], b <- [-999..999] ]
