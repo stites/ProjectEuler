3ce12d88523d1ec5af099802818bdbaeebe184ef
stites <sam@stites.io>
2015-01-26 21:59:39 -0800

    add - bruteforce solution

--- haskell/031.hs
+++ haskell/031.hs
@@ -1,15 +1,25 @@
+-- Coin sums
+-- Problem 31
+-- In England the currency is made up of pound, £, and pence, p, and there are
+-- eight coins in general circulation:
+--
+-- 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p).
+-- It is possible to make £2 in the following way:
+--
+-- 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
+-- How many different ways can £2 be made using any number of coins?
+
+tgt = 200
 opts = [1, 2, 5, 10, 20, 50, 100, 200]
-len  = length opts
-sets = () -- where we store final results
-checkSets = -- simple check to see if val is in sets
-sumSet = -- alias to summing the set
-coinSums current count =
-  -- if sum current == 200 then
-    -- if checkSets current
-    -- then count
-    -- else
-      -- add to set
-      -- count + 1
-  -- else if sum current > 200
-    -- then count
-    -- else add an opt
+
+isOption s = if(s == 200) then 1 else 0
+
+bruteForce = sum [isOption(a+b+c+d+e+f+g+h) |
+                                  a <- opts,
+                                  b <- opts,
+                                  c <- opts,
+                                  d <- opts,
+                                  e <- opts,
+                                  f <- opts,
+                                  g <- opts,
+                                  h <- opts]
