d88b0072ae6d5589f8becf85952701b50acb17cc
stites <sam@stites.io>
2015-02-10 16:36:57 -0800

    wip - broken implimentation of good logic

--- haskell/036.hs
+++ haskell/036.hs
@@ -8,3 +8,17 @@
 -- (Please note that the palindromic number, in either base, may not include
 -- leading zeros.)
 import Euler
+
+cap = 1000000
+
+solve36 val =
+  let
+    subroutine n sum = if (n < 0)
+                       then sum
+                       else if ((isPal n) && (isPal binary n))
+                            then subroutine (n-1) (sum+n)
+                            else subroutine (n-1) sum
+  in
+    subroutine val 0
+
+solution = solve36 $ cap-1
