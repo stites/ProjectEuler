4568ab791e45069f3db95e77e29f7f9dea5e7317
stites <sam@stites.io>
2015-02-23 15:54:05 -0800

    wip - stuck on 39

--- scala/039.scala
+++ scala/039.scala
@@ -9,3 +9,23 @@
  *
  * For which value of p ≤ 1000, is the number of solutions maximised?
  */
+import scala.collection.mutable.ListBuffer
+
+def isRight (a:Int, b:Int, c:Int): Boolean = { a*a + b*b == c*c }
+
+def getSides (perimeter:Int): Int = {
+  var sides = ListBuffer<_>();
+
+  for (a <- 1 to perimeter/2){
+    for (b <- 1 to a) {
+      var c = perimeter-a-b;
+
+      if (isRight(a,b,c)) {
+        sides += List(a,b,c)
+      }
+    }
+  }
+
+  return 1
+}
+val example = getSides(120)
