262fee0ac8b255280876bcd7fe962fd20c5c96e0
stites <sam@stites.io>
2015-02-26 22:08:24 -0800

    moving window solution to 40

--- scala/040.scala
+++ scala/040.scala
@@ -15,3 +15,27 @@
  * d_1 × d_10 × d_100 × d_1000 × d_10000 × d_100000 × d_1000000
  *
  */
+
+// d_1 000 000
+val cap = 1000000
+var currentWindow = "1"
+var len = 0
+var solution = 1
+var x = 1
+var nextMod = 1
+var windowSize = 0
+var cached = 0
+while (len < cap) {
+  currentWindow = x.toString
+  len += currentWindow.length
+  for (a <- x.toString.length to len) {
+    cached = a
+    if (a % nextMod == 0) {
+      windowSize = a - currentWindow.length
+      nextMod *= 10
+      solution *= currentWindow.charAt(windowSize).toInt
+    }
+  }
+  x += 1
+}
+
