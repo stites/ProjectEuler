6ad17923e29e2e4e960ca9b597645a669965ef8f
stites <sam@stites.io>
2015-03-05 07:11:54 -0800

    rm - seive from 37

--- scala/src/main/scala/solutions/037.scala
+++ scala/src/main/scala/solutions/037.scala
@@ -13,32 +13,10 @@
  * NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
  */
 
-import scala.annotation.tailrec
 import scala.collection.mutable.ListBuffer
-import scala.collection.parallel.mutable
-import scala.compat.Platform
 import java.lang.Integer.parseInt
 
-object GenuineEratosthenesSieve extends App {
-  def sieveOfEratosthenes(limit: Int) = {
-    val (primes: mutable.ParSet[Int], sqrtLimit) = (mutable.ParSet.empty ++ (2 to limit), math.sqrt(limit).toInt)
-    @tailrec
-    def prim(candidate: Int): Unit = {
-      if (candidate <= sqrtLimit) {
-        if (primes contains candidate) primes --= candidate * candidate to limit by candidate
-        prim(candidate + 1)
-      }
-    }
-    prim(2)
-    primes
-  }
-  // BitSet toList is shuffled when using the ParSet version. So it has to be sorted before using it as a sequence.
-
-  assert(sieveOfEratosthenes(15099480).size == 976729)
-  println(s"Successfully completed without errors. [total ${Platform.currentTime - executionStart} ms]")
-}
-
-val primes = GenuineEratosthenesSieve.sieveOfEratosthenes(1000000)
+val primes = sieveOfEratosthenes(1000000)
 val elevenPrimes = new ListBuffer[Int]()
 var index = 9
 
